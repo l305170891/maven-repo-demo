d6e85e9c72bb44971f63aedf2e3bcdbe6199a65e
luojian <305170891@qq.com>
2019-04-28 17:58:18 +0800

    commit

---
+++ src/main/java/com/luojian/github/repo/demo/Utils.java
@@ -0,0 +1,10 @@
+package com.luojian.github.repo.demo;
+
+/**
+ * Created by luojian on 2019/4/28.
+ */
+public class Utils {
+    public static int add(int a, int b){
+        return a+b;
+    }
+}
